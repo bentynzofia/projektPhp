5f9f4e0fed4f18589736aed7b0ab59c26c043e4a
bentynzofia <zofia.bentyn@gmail.com>
2019-10-24 17:14:39 +0200

    login html 1

---
+++ logIn.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>login</title>
+    <link rel="stylesheet" href="/css/login.css">
+  </head>
+  <body>
+    <div class="content">
+      <div class="image">
+
+      </div>
+      <div class="login">
+        <input type="text" name="username" value="">
+        <input type="password" name="password" value="">
+        <input type="submit" name="submit" value="Let's get started!">
+
+      </div>
+
+    </div>
+  </body>
+</html>
