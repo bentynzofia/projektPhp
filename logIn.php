9e3d2ce8d35fb4f9db4155d9fe8540e0dd80ab8f
bentynzofia <zofia.bentyn@gmail.com>
2019-10-24 17:27:42 +0200

    login first css try

--- logIn.php
+++ logIn.php
@@ -3,16 +3,18 @@
   <head>
     <meta charset="utf-8">
     <title>login</title>
-    <link rel="stylesheet" href="/css/login.css">
+    <link rel="stylesheet" href="css/login.css">
   </head>
   <body>
     <div class="content">
       <div class="image">
+        <h1>Iknow</h1>
+        <h3>do you tho</h3>
 
       </div>
       <div class="login">
-        <input type="text" name="username" value="">
-        <input type="password" name="password" value="">
+        <input type="text" name="username" autocomplete="off" autofocus><br />
+        <input type="password" name="password" autocomplete="off"><br />
         <input type="submit" name="submit" value="Let's get started!">
 
       </div>
