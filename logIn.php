c89b16d668c3705cda850d2f6e8ec00cbdcd7914
bentynzofia <zofia.bentyn@gmail.com>
2019-10-28 21:23:47 +0100

    fonts updated

--- logIn.php
+++ logIn.php
@@ -4,10 +4,12 @@
     <meta charset="utf-8">
     <title>login</title>
     <link rel="stylesheet" href="css/login.css">
+    <link href="https://fonts.googleapis.com/css?family=Just+Me+Again+Down+Here|Oswald:300,600,700&display=swap" rel="stylesheet">
   </head>
   <body>
     <div class="content">
       <div class="image">
+        <img src="css/img/loginPic.png" alt="">
         <h1>Iknow</h1>
         <h3>do you tho</h3>
 
