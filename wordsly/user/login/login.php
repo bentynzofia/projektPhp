730144e1e71985370f1e2ab1ae5e15177e073dc2
bentynzofia <zofia.bentyn@gmail.com>
2019-11-24 15:47:00 +0100

    login i sign in w pracy

---
+++ wordsly/user/login/login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../../css/user/login.css">
+    <title></title>
+  </head>
+  <body>
+    <div class="content">
+      <div class="text">
+        <h3 class="logo">
+          wordsly
+        </h3>
+      </div>
+      <div class="studentInterface">
+
+      </div>
+      <div class="studentContain">
+        <input type="text" name="" value="">
+        <input type="text" name="" value="">
+        <input type="submit" id="formSub" name="" value="login">
+        <button type="button"id="formSub left" name="button">sign in</button>
+        </button>
+
+      </div>
+      </div>
+
+    </div>
+  </body>
+</html>
